24a0abad4181b916ad3f9f2274df47a1ccfc4afb
Guptahariom28 <hariomgupta2809@gmail.com>
2026-01-31 01:16:40 +0530

    validation has added in the user schema

--- Models/UpsertUserDto.cs
+++ Models/UpsertUserDto.cs
@@ -1,9 +1,17 @@
-﻿namespace apisApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apisApp.Models
 {
     public class UpsertUserDto
     {
         public Guid? Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email")]
         public string Email { get; set; }
     }
 }
